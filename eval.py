42aafbd0d55a869d5dfddaa5827232590c975dc4
assafpin <assaf.pinhasi@gmail.com>
2023-10-22 19:13:44 +0300

    add wandb to eval script

--- eval.py
+++ eval.py
@@ -1,5 +1,6 @@
 
 import sys
+import wandb
 import torch
 import logging
 import multiprocessing
@@ -15,6 +16,9 @@ def eval():
     torch.backends.cudnn.benchmark = True  # Provides a speedup
 
     args = parser.parse_arguments()
+    if args.wandb:
+        wandb.init(project=args.wandb, config=args)
+
     start_time = datetime.now()
     output_folder = f"logs/{args.save_dir}/{start_time.strftime('%Y-%m-%d_%H-%M-%S')}"
     commons.make_deterministic(args.seed)
@@ -37,12 +41,23 @@ def eval():
                      "Evaluation will be computed using randomly initialized weights.")
 
     model = model.to(args.device)
+    if args.wandb:
+        wandb.watch(model)
 
-    test_ds = TestDataset(args.test_set_folder, queries_folder="queries",
-                          positive_dist_threshold=args.positive_dist_threshold)
-
-    recalls, recalls_str = test.test(args, test_ds, model, args.num_preds_to_save, args.image_size_dimension or None)
+    test_ds = TestDataset(f"{args.test_dataset_folder}", queries_folder="queries_v1")
+    recalls, recalls_str = test.test(args=args, eval_ds=test_ds, model=model, resize=args.image_size_dimension)
     logging.info(f"{test_ds}: {recalls_str}")
+    if args.wandb:
+        wandb.log({
+            'Test_Recall@1': recalls[0]
+        })
+
+        logging.info("Experiment finished (without any errors)")
+
+        # 3. Close wandb
+        wandb.finish()
+
+    logging.info("Experiment finished (without any errors)")
 
 
 if __name__ == '__main__':
